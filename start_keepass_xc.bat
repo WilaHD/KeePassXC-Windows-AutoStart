6b8499199d0025558d8cf4249014039445dfbc98
Wila <wilhelmrosinski@gmail.com>
2020-05-10 00:47:38 +0200

    Create start_keepass_xc.bat

---
+++ start_keepass_xc.bat
@@ -0,0 +1,7 @@
+set keepassfolder=C:\Program Files\KeePassXC\KeePassXC.exe
+set databasepath=C:\Path\to\Database.kdbx
+set password=yourPassword
+
+taskkill.exe /IM keepassxc.exe
+@ping -n 2 localhost> nul
+echo %password%|start "KeePassXC" /B "%keepassfolder%" --pw-stdin "%databasepath%"
